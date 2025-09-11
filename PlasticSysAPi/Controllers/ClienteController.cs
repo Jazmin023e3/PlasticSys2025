7f9dff221b1786143b74eae1d77c0472e3b3ad1f
StefaniePaiz <ps25001@esfe.agape.edu.sv>
2025-09-11 14:36:51 -0600

    Se codifico controller

---
+++ PlasticSysAPi/Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlasticSysAPi.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
