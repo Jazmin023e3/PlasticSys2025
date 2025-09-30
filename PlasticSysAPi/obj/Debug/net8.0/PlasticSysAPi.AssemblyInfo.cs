6eaf78d3a30864fe55b5545a2a40594715ebab9d
Jazmin Aguilar <Jazmin086441@outlook.es>
2025-09-29 19:54:37 -0600

    se sigue modificando

--- PlasticSysAPi/obj/Debug/net8.0/PlasticSysAPi.AssemblyInfo.cs
+++ PlasticSysAPi/obj/Debug/net8.0/PlasticSysAPi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlasticSysAPi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+987654043f329aecd14685eb784bd97ac9aff27e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19840eef74b67ece8d5598798a77c731ad4c6827")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlasticSysAPi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlasticSysAPi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
